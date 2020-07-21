44b11adec8c13bdfd9de77320f54d881306c7731
Satyen Verma <sverma44@asu.edu>
2020-07-21 12:26:31 -0700

    Added route for create contact screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CreateContact from './CreateContact'
 class App extends Component {
   state = {
     contacts: [],
-    screen: 'create'
+    screen: 'list'
   }
   
   componentDidMount(){
@@ -35,11 +35,15 @@ class App extends Component {
           <ListContacts 
             contacts={this.state.contacts}
             onDeleteContact = {this.removeContact}
+            onNavigate = {()=>{
+              this.setState(()=> ({
+                screen: 'create'
+              }))
+            }}
           />
         )}
         {this.state.screen === 'create' && (
-          <CreateContact
-          />
+          <CreateContact/>
         )}
       </div>
     );
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -22,7 +22,7 @@ class ListContacts extends Component {
 
     render() {
         const { query } = this.state
-        const { contacts, onDeleteContact } = this.props
+        const { contacts, onDeleteContact, onNavigate } = this.props
         
         const showingContacts = query === '' 
         ? contacts 
@@ -40,6 +40,11 @@ class ListContacts extends Component {
                         value = {query}
                         onChange = {(event)=>this.updateQuery(event.target.value)}
                     />
+                    <a
+                        href='#create'
+                        onClick={onNavigate}
+                        className='add-contact'
+                    >add</a>
                 </div>
 
                 {showingContacts.length!== contacts.length && (
