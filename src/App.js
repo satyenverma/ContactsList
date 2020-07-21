bb8dee8614d8d8d28c41b7b090170a313086e411
Satyen Verma <sverma44@asu.edu>
2020-07-21 12:59:07 -0700

    Use Routes to control page content

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
     contacts: [],
-    screen: 'list'
   }
   
   componentDidMount(){
@@ -31,20 +31,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts 
-            contacts={this.state.contacts}
-            onDeleteContact = {this.removeContact}
-            onNavigate = {()=>{
-              this.setState(()=> ({
-                screen: 'create'
-              }))
-            }}
+          contacts={this.state.contacts}
+          onDeleteContact = {this.removeContact}
           />
         )}
-        {this.state.screen === 'create' && (
-          <CreateContact/>
-        )}
+        />
+
+        <Route path='/create' component={CreateContact}/>
+
       </div>
     );
   }
